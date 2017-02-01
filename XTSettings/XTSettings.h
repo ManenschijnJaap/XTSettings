cc25a0d5889cc66b665f1c996930b5d2b01eae6b
Jaap Manenschijn <jaap@myorder.nl>
2017-02-01 15:00:25 +0100

    initial

---
+++ XTSettings/XTSettings.h
@@ -0,0 +1,23 @@
+//
+//  XTSettings
+//
+//  Created by Taras Kalapun
+//  Copyright (c) 2012 Xaton. All rights reserved.
+//
+
+@interface XTSettings : NSObject
+
++ (void)presetVersionAndDefaults;
++ (void)presetVersionAndDefaultsOnKey:(NSString *)key;
+
++ (void)setSettingsBundleDefaults;
++ (void)setSettingsBundleDefaultsForFile:(NSString *)plistFileName;
+
++ (void)synchronize;
+
++ (id)settingsForKey:(NSString *)key;
+
+#pragma mark - Random helpers
++ (void)listFonts;
+
+@end
